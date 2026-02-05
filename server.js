0a79d795774de302c2ef361b3e1b85dd411484ba
LarissaScarlat <alarissascarlat@gmail.com>
2026-02-05 11:21:00 -0300

    Ajustando detalhes do codigo

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 import express from "express";
-app.use(express.json());
-
 const app = express();
 
+app.use(express.json());
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.sendFile("index.html", { root: "public" });
 });
 
+app.post("/api/perdas", (req, res) => {
+  res.json({ ok: true });
+});
+
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log("Servidor rodando"));
+app.listen(PORT);
